ac19427269c713816a916078ce86a9e7c8925471
zxmon-code <simonnomis.fr@gmail.com>
2023-02-17 22:50:29 +0100

    added help and google search

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+let defaultSearchEngine = "https://duckduckgo.com/";
+// search engines
+let githubSearch = "https://github.com/search?q=";
+let googleSearch = "https://google.com/search?q=";
+let stackoverflowSearch = "https://stackoverflow.com/search?q=";
+
 let input = document.getElementById("input");
 let output = document.getElementById("output");
 
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     // displayInput();
-    handleInput(input.value)
+    handleInput(input.value);
     input.value = "";
+  } else {
+    printText("");
   }
 });
 
@@ -14,24 +22,60 @@ function handleInput(command) {
 }
 
 function commandHandler(command) {
-  if(command.includes("search")){
+  let commandArray = command.split(" ");
+  switch (getCommandUsed(command)) {
+    case "search":
+      console.log("commandHandler working");
+      searchCommand(commandArray);
+    case "help":
+      helpCommand(commandArray);
+    default:
+      helpCommand(["help"]);
+  }
+  if (getCommandUsed === "search") {
     console.log("commandHandler working");
-    searchCommand(command.split(" "))
-  };
+    searchCommand(command.split(" "));
+  }
+}
+
+function getCommandUsed(command) {
+  return command.split(" ")[0].toLowerCase();
 }
 
 function searchCommand(commandArray) {
   let searchString = "";
-  commandArray.forEach(element => {
+  commandArray.forEach((element) => {
     if (element.toLowerCase() === "search") {
-      
     } else {
-      searchString += " "+element;
+      searchString += " " + element;
     }
   });
-  window.open("https://duckduckgo.com/"+searchString, '_blank').focus();
+  window.open(defaultSearchEngine + searchString, "_blank").focus();
 }
 
+function googleSearchCommand(commandArray) {
+  let searchString = "";
+  commandArray.forEach((element) => {
+    if (element.toLowerCase() === "search") {
+    } else {
+      searchString += " " + element;
+    }
+  });
+  window.open(googleSearch + searchString, "_blank").focus();
+}
+
+function helpCommand(commandArray) {
+  if (commandArray.length === 1) {
+    printText(
+      "Available Commands:<br> - search [prompt]<br> - google [prompt]<br> - github [prompt]"
+    );
+  } else {
+  }
+}
+
+function printText(text) {
+  output.innerHTML = text;
+}
 function displayInput() {
   output.innerHTML = input.value;
 }
