6108f7ac4a44cd1cd2221e09024b4870a026f5bb
zxmon-code <simonnomis.fr@gmail.com>
2023-02-18 16:04:00 +0100

    startpage as default search engine and github options

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-let defaultSearchEngine = "https://duckduckgo.com/";
 // search engines
+let startpageSearch = "https://www.startpage.com/sp/search?q=";
+let duckduckgoSearch = "https://duckduckgo.com/";
 let githubSearch = "https://github.com/search?q=";
 let googleSearch = "https://google.com/search?q=";
 let stackoverflowSearch = "https://stackoverflow.com/search?q=";
-let nixosPackageSearch = "https://search.nixos.org/packages?query="
+let nixosPackageSearch = "https://search.nixos.org/packages?query=";
+let youtubeSearch = "https://www.youtube.com/results?search_query=";
 
 let input = document.getElementById("input");
 let output = document.getElementById("output");
 
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    // displayInput();
     handleInput(input.value);
     input.value = "";
   } else {
@@ -26,17 +27,43 @@ function commandHandler(command) {
   let commandArray = command.split(" ");
   switch (getCommandUsed(command)) {
     case "search":
-      console.log("commandHandler working");
-      searchCommand(commandArray, commandArray[0]);
+      searchCommand(commandArray, startpageSearch);
       break;
     case "google":
-      searchCommand(commandArray, commandArray[0]);
+      searchCommand(commandArray, googleSearch);
       break;
+    case "gh":
     case "github":
-      searchCommand(commandArray, commandArray[0]);
+      switch (commandArray[1]) {
+        case "user":
+          commandArray.push("&type=users");
+          commandArray.splice(1, 1);
+          console.log(commandArray)
+          break;
+        case "repos":
+        case "repositories":
+          commandArray.push("&type=repositories");
+          commandArray.splice(1, 1);
+          break;
+        case "code":
+          commandArray.push("&type=code");
+          commandArray.splice(1, 1);
+          break;
+        default:
+          break;
+      }
+      searchCommand(commandArray, githubSearch);
       break;
     case "nixos":
-      searchCommand(commandArray, commandArray[0]);
+      searchCommand(commandArray, nixosPackageSearch);
+      break;
+    case "yt":
+    case "youtube":
+      searchCommand(commandArray, youtubeSearch);
+      break;
+    case "ddg":
+    case "duckduckgo":
+      searchCommand(commandArray, duckduckgoSearch);
       break;
     case "help":
       helpCommand(commandArray);
@@ -51,37 +78,25 @@ function getCommandUsed(command) {
 
 function searchCommand(commandArray, searchEngine) {
   let searchString = "";
-  commandArray.forEach((element) => {
-    if (element.toLowerCase() === searchEngine) {
+  for (let i = 0; i < commandArray.length; i++) {
+    if (i === 0) {
     } else {
-      searchString += " " + element;
+    const element = commandArray[i];
+    searchString += " " + element;
     }
-  });
-  switch (searchEngine) {
-    case "search":
-      console.log("fuck");
-      window.open(defaultSearchEngine + searchString, "_blank").focus();
-      break;
-    case "google":
-      console.log("google");
-      window.open(googleSearch + searchString, "_blank").focus();
-      break;
-    case "github":
-      window.open(githubSearch + searchString, "_blank").focus();
-      break;
-    case "nixos":
-      window.open(nixosPackageSearch + searchString, "_blank").focus();
-      break;
-    default:
-      break;
-  }
-  
+  } 
+  window.open(searchEngine + searchString, "_blank").focus();
+  printText("searching...");
 }
 
 function helpCommand(commandArray) {
   if (commandArray.length === 1) {
     printText(
-      "Available Commands:<br> - search [prompt]<br> - google [prompt]<br> - github [prompt]"
+      `Available Commands:<br>
+      - search [prompt]<br>
+      - google [prompt]<br>
+      - github [prompt]<br>
+      - youtube [prompt]<br>`
     );
   } else {
   }
